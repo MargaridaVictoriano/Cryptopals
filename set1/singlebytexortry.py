e4f53211537f2c6ab3528be106cbf79b657adf47
Margarida Victoriano <38563697+MargaridaVictoriano@users.noreply.github.com>
2019-03-23 15:15:19 +0000

    Update singlebytexortry.py

--- set1/singlebytexortry.py
+++ set1/singlebytexortry.py
@@ -1,3 +1,4 @@
+#this script gives all possibilities, the "singlebytexor.py" gives the right output.
 def singlebytexortry():
     s = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".decode("hex")
     for i in range(0,256):
