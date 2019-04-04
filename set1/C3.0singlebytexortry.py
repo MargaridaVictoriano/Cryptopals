9a49b9dbe7e45095122b33b31bbb7b2fc2928236
Margarida Victoriano <38563697+MargaridaVictoriano@users.noreply.github.com>
2019-04-04 18:57:13 +0100

    Delete C3.0singlebytexortry.py

--- set1/C3.0singlebytexortry.py
+++
@@ -1,7 +0,0 @@
-#this script gives all possibilities, the "singlebytexor.py" gives the right output.
-def singlebytexortry():
-    s = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".decode("hex")
-    for i in range(0,256):
-        st = "".join(chr(ord(s[k])^i) for k in range(0,len(s)))
-        print st
-singlebytexortry()
